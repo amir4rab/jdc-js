bff781ca55e279867a89d87ba95e6e0f69f326a3
Amir <60965542+amir4rab@users.noreply.github.com>
2024-09-13 06:18:17 +0000

    Added utilities for converting Jalaali dates into UNIX timestamp

---
+++ utilities/convert.mjs
@@ -0,0 +1,118 @@
+/**
+ * This file contains Utilities for Converting a 
+ * Jalaali date into the UNIX milliseconds timestamp
+ */
+
+import {
+  cycleBreakers,
+  jalaaliMonths,
+  isLeap,
+  leapYearsUntil,
+} from "./jalaali.mjs";
+import {
+  gregorianLeapYearsUntil,
+  jalaaliEpochInCommonEra,
+} from "./gregorian.mjs";
+
+/** ---------------- **/
+/** -- Vairables  -- **/
+/** ---------------- **/
+
+/**
+ * Milliseconds in one day
+ * 24h * 60m * 60s * 1000ms
+ */
+const msInDay = 24 * 60 * 60 * 1000;
+
+/** ---------------- **/
+/** -- Utilites   -- **/
+/** ---------------- **/
+
+/**
+ * Calculates the difference of leap years of Jalaali Calender and Gregorian Calender until the given year
+ * @param {int} year
+ * @returns {int}
+ */
+const diffInLeapYears = (year) => {
+  const jalaaliLeapYears = leapYearsUntil(year);
+  const gregorianLeapYears = gregorianLeapYearsUntil(
+    year + jalaaliEpochInCommonEra
+  );
+
+  const diff = jalaaliLeapYears - gregorianLeapYears;
+
+  return diff;
+};
+
+/**
+ * Calculates the day of Nowruz in UNIX milliseconds
+ * @param {int} year
+ * @returns {int}
+ */
+export const getNowruz = (year) => {
+  const leapYears = diffInLeapYears(year);
+  const nowruz = 6 + leapYears;
+
+  // Converting the Nowruz date into a string
+  const nowruzStr = nowruz.toString().padStart(2, "0");
+
+  // Getting the timestamp of the Nowruz day
+  const nowruzTimestamp = new Date(
+    year + jalaaliEpochInCommonEra + "-03-" + nowruz
+  ).valueOf();
+
+  return nowruzTimestamp;
+};
+
+/**
+ * Converts the given Jalaali date into the UNIX milliseconds equivalent
+ * @param {number} year
+ * @param {number} month
+ * @param {number} day
+ * @returns {[int, null | error]}
+ */
+export const convertDate = (year, month, day) => {
+  // Validating the input types
+  if (
+    typeof year !== "number" ||
+    typeof month !== "number" ||
+    typeof day !== "number"
+  )
+    return [0, new Error("The provided values must be numbers")];
+
+  // Validating the year
+  if (year < cycleBreakers[0] || year > cycleBreakers[cycleBreakers.length - 1])
+    return [0, new Error("The provided year is out of the valid range")];
+
+  // Validating the month
+  if (month < 1 || month > 12)
+    return [0, new Error("The provided month is out of the valid range")];
+
+  // Checking if the current year is a leap year
+  const leap = isLeap(year);
+  const months = leap ? jalaaliMonths.leap : jalaaliMonths.common;
+
+  // Validating the provided day
+  if (day < 1 || day > months[month]) {
+    return [0, new Error("Provided day is out of the valid range")];
+  }
+
+  // Getting the timestamp of Nowruz day
+  let nowruzTimestamp = getNowruz(year);
+
+  /** Total days since Nowruz */
+  let daysSinceNowruz = 0;
+
+  // Adding the length of each past month
+  for (let i = 0; i < month - 1; i++) {
+    daysSinceNowruz += months[i];
+  }
+
+  // Adding the total number of days in the current month
+  daysSinceNowruz += day - 1;
+
+  // Adding the day's value to the Nowruz timestamp
+  nowruzTimestamp += msInDay * daysSinceNowruz;
+
+  return [nowruzTimestamp, null];
+};
