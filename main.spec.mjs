17e75a470e26823dc75a91fa8878d9d5aac6d123
Amir <60965542+amir4rab@users.noreply.github.com>
2024-09-15 05:15:34 +0000

    Updated the test script to run the tests

---
+++ main.spec.mjs
@@ -0,0 +1,9 @@
+"use strict";
+
+// Importing tests
+import convertTests from "./tests/convert.spec.mjs";
+import leapTests from "./tests/leap.spec.mjs";
+
+// Executing tests
+convertTests();
+leapTests();
